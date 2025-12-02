e4ca1bdefc53c7e8173f7f687efd035e9ca248b7
Stephen Kelman <stephenrkelman@gmail.com>
2025-12-01 18:08:57 -0800

    day 1 part 2

--- 1/run.py
+++ 1/run.py
@@ -5,10 +5,15 @@ cur_pos = 50
 num_zeros = 0
 
 for line in lines:
+    prev_pos = cur_pos
     if line[0] == 'R':
         cur_pos += int(line[1:])
     else:
         cur_pos -= int(line[1:])
+    lower = min(cur_pos, prev_pos)
+    upper = max(cur_pos, prev_pos)
+    zeros_between = (upper - 1)//100 - lower//100
+    num_zeros += zeros_between
     cur_pos %= 100
     if cur_pos == 0:
         num_zeros += 1
